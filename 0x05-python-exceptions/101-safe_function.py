ad1cf57213fd3c715486aa8e2210830c057910bf
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-12-18 05:16:02 -0800

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,42 +1,25 @@
-#include <Python.h>
+#!/usr/bin/python3
 
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-void print_python_float(PyObject *p);
 
-/**
- * print_python_list - Prints basic info about Python lists.
- * @p: A PyObject list object.
- */
-void print_python_list(PyObject *p)
-{
-        Py_ssize_t size, alloc, i;
-        const char *type;
-        PyListObject *list = (PyListObject *)p;
-        PyVarObject *var = (PyVarObject *)p;
+import sys
 
-        size = var->ob_size;
-        alloc = list->allocated;
 
-        fflush(stdout);
 
-        printf("[*] Python list info\n");
-        if (strcmp(p->ob_type->tp_name, "list") != 0)
-        {
-                printf("  [ERROR] Invalid List Object\n");
-                return;
-        }
 
-        printf("[*] Size of the Python List = %ld\n", size);
-        printf("[*] Allocated = %ld\n", alloc);
+def safe_function(fct, *args):
+    """Executes a function safely.
+    Args:
+        fct: The function to execute.
+        args: Arguments for functions.
+    Returns:
+        If an error occurs - None.
+        Otherwise - the result of the call to fct.
+    """
 
-        for (i = 0; i < size; i++)
-        {
-                type = list->ob_item[i]->ob_type->tp_name;
-                printf("Element %ld: %s\n", i, type);
-                if (strcmp(type, "bytes") == 0)
-                        print_python_bytes(list->ob_item[i]);
-                else if (strcmp(type, "float") == 0)
-                        print_python_float(list->ob_item[i]);
-        }
-}
+
+    try:
+        result = fct(*args)
+        return (result)
+    except Exception:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
