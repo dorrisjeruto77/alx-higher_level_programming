d730e2efc579a4e827293144b569681ed1459991
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2024-01-14 12:46:41 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 
 """
-Module documentation: LockedClass module
+Module documentation: LockedClass module.
 This module provides the LockedClass class which restricts attribute creation.
 """
 
 class LockedClass:
     """
     A class that only allows creating an attribute named 'first_name'.
-    Any attempt to set other attributes will result in an AttributeError.
+    Attempts to set or access other attributes result in an AttributeError.
     """
+
+    __slots__ = ['first_name']
 
     def __setattr__(self, name, value):
         if name == "first_name":
             super().__setattr__(name, value)
         else:
-            raise AttributeError(f"Can't set attribute '{name}'")
+            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+
+    def __getattr__(self, name):
+        raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
 
 # Example Usage:
 # obj = LockedClass()
 # obj.first_name = "Alex"  # This will work
 # obj.last_name = "Smith"  # This will raise an AttributeError
-# print(obj.__dict__)      # Will display the dictionary of obj
+# print(obj.__dict__)      # This will raise an AttributeError
