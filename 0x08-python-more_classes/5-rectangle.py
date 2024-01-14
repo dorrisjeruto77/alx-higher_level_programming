2aa9d69c8cc4d59feebbcc5141cc124e26d544a9
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2024-01-14 09:59:48 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 
 class Rectangle:
@@ -14,7 +13,7 @@ class Rectangle:
     def __exit__(self, exc_type, exc_val, exc_tb):
         print("Bye rectangle...")
 
-# Usage Example
-with Rectangle(2, 4) as myrectangle:
-    # Do something with myrectangle
-    pass  # This pass statement is just a placeholder
+# Correct usage in a loop with context manager
+for i in range(10):
+    with Rectangle(2, 4) as m1:
+        pass  # This pass statement is just a placeholder
