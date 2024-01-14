7c544f9a1622ec5966cf64bd34f9075df4832b28
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2024-01-14 09:41:05 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -7,18 +7,13 @@ class Rectangle:
 
     # [rest of the class definition with properties and methods]
 
-    def __del__(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
         print("Bye rectangle...")
 
-# Usage Example 1
-try:
-    myrectangle = Rectangle(2, 4)
-    del myrectangle
-    print(myrectangle.width)  # This will raise an error
-except Exception as e:
-    print(f"[{e.__class__.__name__}] {e}")
-
-# Usage Example 2 (Loop)
-for i in range(10):
-    m1 = Rectangle(2, 4)
-    del m1
+# Usage Example
+with Rectangle(2, 4) as myrectangle:
+    # Do something with myrectangle
+    pass  # This pass statement is just a placeholder
