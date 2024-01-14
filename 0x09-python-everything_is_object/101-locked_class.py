f31e50cbc5d93b76d5322dedeb1e02c97ff74d56
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2024-01-14 12:25:20 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
 class LockedClass:
-    __slots__ = ("first_name",)
+    """
+    A class that only allows creating an attribute named 'first_name'.
+    """
+    def __setattr__(self, name, value):
+        if name == "first_name":
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError(f"Can't set attribute '{name}'")
 
-    def __init__(self, first_name):
-        self.first_name = first_name
-
-    def __setattr__(self, attr, value):
-        if attr != 'first_name':
-            raise AttributeError(f"'LockedClass' object has no attribute '{attr}'")
-        super().__setattr__(attr, value)
-
-if __name__ == "__main__":
-    # Example usage
-    obj1 = LockedClass("Alex")
-    obj1.age = 30  # This will raise an AttributeError
+# Example Usage:
+obj = LockedClass()
+obj.first_name = "Alex"  # This will work
+obj.last_name = "Smith"  # This will raise an AttributeError
